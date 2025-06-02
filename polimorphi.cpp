b3b2cbb4a7eb644967344dddfcb1910ece4d9289
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-06-02 19:04:27 +0700

    finalize main function with return 0

--- polimorphi.cpp
+++ polimorphi.cpp
@@ -1,35 +1,6 @@
-#include <iostream>
-using namespace std;
 
-class sesorang {
-public:
-// virtual void pesan()=0;
-virtual void pesan() {
-    cout << "Pesan dari kelas sesorang" << endl;
+    obyek = &b;
+    obyek->pesan(); // Output: Pesan dari lia
+    return 0;
 }
-};
 
-class joko : public sesorang {
-public:
-void pesan() override {
-    cout << "Pesan dari joko" << endl;
-
-}
-};
-
-class lia : public sesorang {
-public:
-void pesan() override {
-    cout << "Pesan dari kelas lia" << endl;
-}
-};
-
-int main(){
-    sesorang *obyek;
-    joko a;
-    lia b;
-   
-    obyek = &a;
-    obyek->pesan(); // Output: Pesan dari joko
-
-   
