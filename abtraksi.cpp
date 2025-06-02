791910320d6f8869eb0c0bfa41d38e509527ad2a
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-06-02 18:26:21 +0700

    add file abstraksi

--- abtraksi.cpp
+++ abtraksi.cpp
@@ -1,2 +1,29 @@
 #include<iostream>
 using namespace std;
+
+class Abstraksiklass{
+    private:
+    string  x,y;
+
+    public:
+// method untuk mengali nilai
+//private member
+void setxy(string a, string b) {
+    x = a;
+    y = b;
+}
+// mentaplikan nilai
+void display() {
+    cout << "Nilai x: " << x << endl;
+    cout << "Nilai y: " << y << endl;
+}
+};      
+int main() {
+    Abstraksiklass ak;
+    ak.setxy("yogyakarta", "kampus");
+    //menamkilan nilai
+    ak.display();
+
+    return 0;
+}
+
