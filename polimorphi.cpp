d8707a794d4082688ac58fb74741084398ac9e9c
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-06-02 18:54:39 +0700

    base class bash

--- polimorphi.cpp
+++ polimorphi.cpp
@@ -3,37 +3,5 @@ using namespace std;
 
 class sesorang {
 public:
-// virtual void pesan()=0;
-virtual void pesan() {
-    cout << "Pesan dari kelas sesorang" << endl;
-}
-};
 
-class joko : public sesorang {
-public:
-void pesan(){
-    cout << "Pesan dari joko" << endl;
-
-}
-};
-
-class lia : public sesorang {
-public:
-void pesan()  {
-    cout << "Pesan dari kelas lia" << endl;
-}
-};
-
-int main(){
-    sesorang *obyek;
-    joko a;
-    lia b;
-   
-    obyek = &a;
-    obyek->pesan(); // Output: Pesan dari joko
-
-    obyek = &b;
-    obyek->pesan(); // Output: Pesan dari lia
-    return 0;
-}
 
